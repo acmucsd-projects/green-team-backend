d3b7d9d04415db7633673469a04e422ad6fe71cc
shravanhariharan2 <smhariha@ucsd.edu>
2020-05-15 16:37:38 -0700

    implement profile pic uploads for trees

--- app/api/treeRouter.py
+++ app/api/treeRouter.py
@@ -3,6 +3,8 @@ from flask import request, json, jsonify, Blueprint
 from ..services import treeService
 from ..storage.s3 import upload_file
 
+import os
+
 tree_blueprint = Blueprint('tree_blueprint', __name__, url_prefix="/trees")
 
 @tree_blueprint.route("/<tree_id>", methods=["GET"])
@@ -22,8 +24,12 @@ def addPoints():
 
 @tree_blueprint.route("/picture", methods=["POST"])
 def uploadPicture():
-    file = request.get_json()
+    file = request.files["image"]
     if not file:
         return "Error: No file provided"
-    file_url = upload_file(file) 
+    
+    file.save(os.path.join("/tmp/", file.filename))
+    file_url = upload_file(file.filename)
+    
+    return { "message" : "File uploaded successfully", "profile_url": file_url }
 
--- app/storage/s3.py
+++ app/storage/s3.py
@@ -1,9 +1,22 @@
 import boto3
+from botocore.client import Config
+from botocore import UNSIGNED
+import os
 
 def upload_file(file_name):
+    bucket = os.environ["AWS_S3_BUCKET"]
     object_name = "bit-byte/profiles/" + file_name
     
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+    )
     
-    response = s3_client.upload_file(file_name, bucket, object_name)
-    return response
+    s3_client.upload_file('/tmp/' + file_name, bucket, object_name)
+
+    config = Config(signature_version=UNSIGNED)
+    config.signature_version = UNSIGNED
+    profile_url = boto3.client('s3', config=config).generate_presigned_url('get_object', ExpiresIn=0, Params={'Bucket': bucket, 'Key': object_name})
+    
+    return profile_url

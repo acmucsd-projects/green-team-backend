925767e40057b10b5d0d977ba11c9639f23e78de
shravanhariharan2 <smhariha@ucsd.edu>
2020-05-23 11:46:42 -0700

    Add error handling for routes

--- app/api/nodeRouter.py
+++ app/api/nodeRouter.py
@@ -6,29 +6,40 @@ node_blueprint = Blueprint('node_blueprint', __name__, url_prefix="/nodes")
 
 @node_blueprint.route("/", methods=["GET"])
 def getAllNodes():
-    nodes = nodeService.getAllNodes()
-    return { "error": None, "nodes": nodes }
-
+    try:
+        nodes = nodeService.getAllNodes()
+        return { "error": None, "nodes": nodes }
+    except:
+        return { "error": "Error fetching nodes" }
 @node_blueprint.route("/", methods=["POST"])
 def postNode():
-    node = request.get_json()
-    if not node:
-        return {"error" : "No request body provided"}
-    new_node = nodeService.postNode(node)
-    return { "error": None, "node": new_node }
+    try:
+        node = request.get_json()
+        if not node:
+            return { "error" : "No request body provided" }
+        new_node = nodeService.postNode(node)
+        return { "error": None, "node": new_node }
+    except:
+        return { "error": "Error creating node" }
 
 @node_blueprint.route("/<node_id>", methods=["GET"])
 def getNode(node_id):
-    node = nodeService.getNodeById(node_id)
-    if not node:
-        return {"error" : "Unable to fetch node from database"}
-    return { "error": None, "node": node}
+    try:
+        node = nodeService.getNodeById(node_id)
+        if not node:
+            return {"error" : "Unable to fetch node from database"}
+        return { "error": None, "node": node}
+    except:
+        return { "error": "Error fetching node" }
 
 @node_blueprint.route("/<node_id>/children", methods=["GET"])
 def getNodeAndChildren(node_id):
-    nodes = nodeService.getNodeAndChildren(node_id, levels=1)
-    if not nodes:
-        return {"error" : "Unable to fetch nodes from database"}
-    return { "error": None, "nodes": nodes}
+    try:
+        nodes = nodeService.getNodeAndChildren(node_id, levels=1)
+        if not nodes:
+            return {"error" : "Unable to fetch nodes from database"}
+        return { "error": None, "nodes": nodes}
+    except:
+        return { "error": "Error fetching nodes" }
 
         
--- app/api/treeRouter.py
+++ app/api/treeRouter.py
@@ -3,6 +3,7 @@ from flask import request, json, jsonify, Blueprint
 from ..services import treeService
 from ..storage.s3 import upload_file
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
 
 import uuid
 import os
@@ -11,41 +12,57 @@ tree_blueprint = Blueprint('tree_blueprint', __name__, url_prefix="/trees")
 
 @tree_blueprint.route("/", methods=["GET"])
 def getTreesAsc():
-    trees = treeService.getTreesAsc()
-    return { "error": None, "trees": trees }
+    try:
+        trees = treeService.getTreesAsc()
+        return { "error": None, "trees": trees }
+    except:
+        return { "error": "Error fetching trees" }
 
 @tree_blueprint.route("/<tree_id>", methods=["GET"])
 def getTree(tree_id):
-    tree = treeService.getTree(tree_id)
-    if not tree:
-        return {"error" : "Unable to fetch tree from database"}
-    return { "error": None, "tree": tree }
+    try:
+        tree = treeService.getTree(tree_id)
+        if not tree:
+            return {"error" : "Unable to fetch tree from database"}
+        return { "error": None, "tree": tree }
+    except:
+        return { "error": "Error fetching tree" }
 
 @tree_blueprint.route("/<tree_id>", methods=["PUT"])
 def putTree(tree_id):
-    body = request.get_json()
-    if not body:
-        return {"error" : "No request body provided"}
-    tree = treeService.updateTree()
-    return { "error": None, "tree": tree }
+    try:
+        body = request.get_json()
+        if not body:
+            return {"error" : "No request body provided"}
+        tree = treeService.updateTree()
+        return { "error": None, "tree": tree }
+    except:
+        return { "error": "Error updating tree" }
 
 @tree_blueprint.route("/points", methods=["POST"])
 def addPoints():
-    body = request.get_json()
-    if not body:
-        return { "error" : "No request body provided" }
-    tree = treeService.addPointsById(body["id"], body["points"])
-    return { "error": None, "tree": tree }
+    try:
+        body = request.get_json()
+        if not body:
+            return { "error" : "No request body provided" }
+        tree = treeService.addPointsById(body["id"], body["points"])
+        return { "error": None, "tree": tree }
+    except:
+        return { "error": "Error updating tree's points" }
 
 @tree_blueprint.route("/picture", methods=["POST"])
 def uploadPicture():
-    file = request.files["image"]
-    tree_id = request.form.get("tree_id")
-    if not file:
-        return { "error" : "No file provided" }
-    filename = str(uuid.uuid4())
-    file.save(os.path.join("/tmp/", filename))
-    file_url = upload_file(filename)
-    tree = treeService.updateProfileUrl(tree_id, file_url)
-    return { "message" : "File uploaded successfully", "tree": tree }
-
+    try:
+        file = request.files["image"]
+        tree_id = request.form.get("id")
+        if not file:
+            return { "error" : "No file provided" }
+        filename = str(uuid.uuid4())
+        file.save(os.path.join("/tmp/", filename))
+        file_url = upload_file(filename)
+        tree = treeService.updateProfileUrl(tree_id, file_url)
+        return { "message" : "File uploaded successfully", "tree": tree }
+    except RequestEntityTooLarge:
+        return { "error": "File size too large"}
+    except:
+        return { "error": "Error uploading file" }
